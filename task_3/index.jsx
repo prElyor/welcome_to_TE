77b602b4ee7c745be685abf4f1711046ec8aac52
Elyor <elyor.sh.99@mail.ru>
2022-08-24 14:20:36 +0300

    Выполнена третая задача

--- task_3/index.jsx
+++ task_3/index.jsx
@@ -1,8 +1,37 @@
 import { Fragment, memo } from 'react';
 
+// ======= ----  Первая идея ------ ========
+
 const MainComponent = ({
     user = { name: 'unknown', age: null } // default value for `props.user`
 }) => {
+    return (
+        <Fragment>
+            <ChildComponent name={user.name} age={user.age} />
+        </Fragment>
+    );
+};
+
+// memoized component
+const ChildComponent = memo(({ name, age } ) => {
+    return (
+        <div>user name: {name}, user age: {age}</div>
+    )
+});
+
+
+// ======== ---- Вторая идея ----- ========
+
+function areEqual(prevProps, nextProps) {
+    // Самый простой способ сравнивать объекты, массивы это через JSON.stringify()
+    // хотя этот способ не рекомендуют в официальной документации
+    // Можно было сравнивать prevProps.user.name === nextProps.user.name и т.д.
+    return JSON.stringify(prevProps) === JSON.stringify(nextProps);
+}
+
+const MainComponent = ({
+                           user = { name: 'unknown', age: null } // default value for `props.user`
+                       }) => {
     return (
         <Fragment>
             <ChildComponent user={user} />
@@ -15,4 +44,4 @@ const ChildComponent = memo(({ user: { name, age } }) => {
     return (
         <div>user name: {name}, user age: {age}</div>
     )
-});
+}, areEqual);
